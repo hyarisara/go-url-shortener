547505d9b58276284990e93c1b0c8e3bfac26b8d
hyarisara <hyarisara28@gmail.com>
2026-02-11 12:39:10 +0300

    Add web server support for browser redirection

--- main.go
+++ main.go
@@ -2,15 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
+// HTTP handler to redirect shortcodes in browser
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Path[1:] // remove leading /
+	url, err := ExpandURL(code) // use existing ExpandURL
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, url, http.StatusFound)
+}
+
+func startWebServer() {
+	http.HandleFunc("/", redirectHandler)
+	fmt.Println("Web server running at http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage:")
-		fmt.Println("  shorten <url>")
-		fmt.Println("  expand <code>")
-		fmt.Println("  list")
+	// If no CLI args, start web server
+	if len(os.Args) == 1 {
+		startWebServer()
 		return
 	}
 
@@ -26,7 +42,6 @@ func main() {
 			return
 		}
 		fmt.Println("Short code:", code)
-
 	case "expand":
 		if len(os.Args) < 3 {
 			fmt.Println("Provide a code")
@@ -38,10 +53,8 @@ func main() {
 			return
 		}
 		fmt.Println("Original URL:", url)
-
 	case "list":
 		ListURLs()
-
 	default:
 		fmt.Println("Unknown command")
 	}
